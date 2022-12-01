43e6010a70765014920269362280ae2726f8fb2a
Joao <joaop86@hotmail.com>
2022-12-01 17:18:17 +0000

    update landing page

--- fairshift/src/pages/index.tsx
+++ fairshift/src/pages/index.tsx
@@ -4,8 +4,6 @@ import Head from "next/head";
 import Link from "next/link";
 import styles from "./index.module.css";
 
-import { trpc } from "../utils/trpc";
-
 const Home: NextPage = () => {
   return (
     <>
@@ -24,20 +22,9 @@ const Home: NextPage = () => {
               className={styles.card}
               href="https://create.t3.gg/en/usage/first-steps"
             >
-              <h3 className={styles.cardTitle}>First Steps →</h3>
+              <h3 className={styles.cardTitle}>Generate schedule →</h3>
               <div className={styles.cardText}>
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className={styles.card}
-              href="https://create.t3.gg/en/introduction"
-            >
-              <h3 className={styles.cardTitle}>Documentation →</h3>
-              <div className={styles.cardText}>
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
+                Automatic schedule generation based on your preferences.
               </div>
             </Link>
           </div>
@@ -55,17 +42,8 @@ export default Home;
 const AuthShowcase: React.FC = () => {
   const { data: sessionData } = useSession();
 
-  const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  );
-
   return (
     <div className={styles.authContainer}>
-      <p className={styles.showcaseText}>
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
       <button
         className={styles.loginButton}
         onClick={sessionData ? () => signOut() : () => signIn()}
