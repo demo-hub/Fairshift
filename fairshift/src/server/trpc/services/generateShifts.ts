c3f4bbdf61e4f02b1c123c517ca8bcff56f5df73
Joao <joaop86@hotmail.com>
2022-12-04 23:18:07 +0000

    fix generate schedule script

--- fairshift/src/server/trpc/services/generateShifts.ts
+++ fairshift/src/server/trpc/services/generateShifts.ts
@@ -39,13 +39,24 @@ const generateShifts = async ({
     // Calculate the day of the week (1 = Monday, 7 = Sunday)
     const dayOfWeek = (i % 7) + 1;
     // Calculate the shift number (1 = first shift, 2 = second shift, etc.)
-    const shiftNumber = Math.floor(i / shiftsPerDay) + 1;
-    // Add the shift to the schedule
-    shiftSchedule.push({
-      employee: Math.floor(Math.random() * totalEmployees) + 1,
-      dayOfWeek,
-      shiftNumber,
-      hours: hoursPerShift,
+    const shiftNumber =
+      shiftSchedule.filter((s) => s.dayOfWeek === dayOfWeek).length /
+        employeesPerShift +
+      1;
+    // Select random employees for the shift
+    const employeeArray = Array.from(Array(totalEmployees).keys());
+    // Shuffle array
+    employeeArray.sort(() => 0.5 - Math.random());
+    // Get sub-array of first n elements after shuffled
+    const selected = employeeArray.slice(0, employeesPerShift);
+    // Add shifts to the schedule
+    selected.forEach((employee) => {
+      shiftSchedule.push({
+        employee: employee + 1,
+        dayOfWeek,
+        shiftNumber,
+        hours: hoursPerShift,
+      });
     });
   }
 
