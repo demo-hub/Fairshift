88bd50092cf93b54fee4c3071c96c5e5cbdd89d5
Joao <joaop86@hotmail.com>
2022-12-29 15:31:14 +0000

    fix invitation

--- fairshift/src/pages/invite/[teamId].tsx
+++ fairshift/src/pages/invite/[teamId].tsx
@@ -1,4 +1,4 @@
-import { Text } from "@chakra-ui/react";
+import { Text, useToast } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
@@ -7,11 +7,12 @@ import { trpc } from "../../utils/trpc";
 
 const Invitation: NextPage = () => {
   const { data: sessionData, status } = useSession();
+  const toast = useToast();
   const router = useRouter();
   const { teamId } = router.query;
   const joinTeam = trpc.team.joinTeam.useMutation();
   // Check if user is already in a team
-  const { data: userTeam } = trpc.team.getUserTeam.useQuery(
+  const { data: userTeam, isLoading } = trpc.team.getUserTeam.useQuery(
     { userId: sessionData?.user?.id ?? "" },
     { enabled: status === "authenticated" }
   );
@@ -23,14 +24,37 @@ const Invitation: NextPage = () => {
     } else if (status === "authenticated") {
       // Redirect to dashboard if user is already in a team
       if (userTeam?.team?.name) {
+        // Notify user that they are already in a team
+        toast({
+          title: "You are already in a team.",
+          description: "You can only be in one team at a time.",
+          status: "error",
+        });
         router.push("/");
+        return;
+      } else if (!isLoading) {
+        // Join team
+        joinTeam.mutate({ teamId: teamId as string });
+        // Notify user that they have joined the team
+        toast({
+          title: "You have joined the team.",
+          description: "You can now be assigned to schedules.",
+          status: "success",
+        });
+        router.push("/");
+        return;
       }
-
-      // Join team
-      joinTeam.mutateAsync({ teamId: teamId as string });
-      router.push("/");
     }
-  }, [joinTeam, router, sessionData, status, teamId, userTeam?.team?.name]);
+  }, [
+    isLoading,
+    joinTeam,
+    router,
+    sessionData,
+    status,
+    teamId,
+    toast,
+    userTeam?.team?.name,
+  ]);
 
   return joinTeam.isLoading ? (
     <Text color="white">Loading...</Text>
